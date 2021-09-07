92a7087bd16a2d2dbb01e7014c4d31f5a81072aa
Lewis Heptonstall <lhepto@users.noreply.github.com>
2021-09-07 15:45:45 +0100

    MMCA 3040 (#3)
    * Using govuk components for table on daily_statement
    
    * Using govuk components for table on pending_daily_statement.

---
+++ app/views/components/PaymentHeader.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.components
+
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.Aliases.{HeadCell, HtmlContent}
+
+object PaymentHeader {
+
+    def apply()(implicit messages: Messages): Seq[HeadCell] = {
+
+        Seq(
+            HeadCell(
+                content = HtmlContent(
+                    s"""
+                    <abbr title="${messages("cf.cash-account.detail.movement-reference-number")}">
+                    ${messages("cf.cash-account.detail.mrn")}
+                    </abbr>
+                    """)
+            ),
+            HeadCell(
+                content = HtmlContent(
+                    s"""
+                    <abbr title="${messages("cf.cash-account.detail.unique-consignment-reference")}">
+                    ${messages("cf.cash-account.detail.ucr")}
+                    </abbr>
+                    """)
+            ),
+            HeadCell(
+                content = HtmlContent(
+                    s"""
+                    ${messages("cf.cash-account.detail.declarant")}
+                    <abbr title="${messages("cf.cash-account.detail.eori-definition")}">
+                    ${messages("cf.cash-account.detail.eori")}
+                    </abbr>
+                    """)
+            ),
+            HeadCell(
+                format = Some("numeric"),
+                content = HtmlContent(
+                    s"""
+                    ${messages("cf.cash-account.detail.amount")}
+                    """)
+            ),
+        )
+
+    }
+
+}
+
