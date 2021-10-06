11a534cafb25dd5a386b400129a1e7251c061c18
Muhammad <46523489+muhammadibraaheem@users.noreply.github.com>
2021-10-06 16:40:35 +0100

    add test back for accounts with balance

--- test/controllers/CashAccountControllerSpec.scala
+++ test/controllers/CashAccountControllerSpec.scala
@@ -111,6 +111,7 @@ class CashAccountControllerSpec extends SpecBase {
         val result = route(app, request).value
         status(result) mustEqual OK
         contentAsString(result) mustEqual view(CashAccountViewModel(eori, newCashAccount))(request, messages, appConfig).toString()
+        contentAsString(result) must include regex "search and download previous transactions as CSV."
       }
     }
 
