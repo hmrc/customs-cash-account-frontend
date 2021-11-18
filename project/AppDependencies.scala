214bb8abc314f5aa5841e6c062a95b68b2650f39
Derek Palmer <35488891+derekspalmer@users.noreply.github.com>
2021-11-18 11:47:56 +0000

    MMCA-3264: upgrade sbt version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.16.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "1.26.0-play-28",
-    "org.typelevel" %% "cats-core" % "1.5.0-RC1",
+    "org.typelevel" %% "cats-core" % "2.3.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.56.0",
     "org.webjars.npm" % "moment" % "2.27.0",
     "uk.gov.hmrc" %% "tax-year" % "1.6.0"
@@ -18,6 +18,6 @@ object AppDependencies {
     "org.jsoup" % "jsoup" % "1.10.2" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.7.1" % Test
+    "org.mockito" %% "mockito-scala-scalatest" % "1.16.46" % Test
   )
 }
