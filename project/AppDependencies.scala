df85386c61fd69a2d7a0ba96dcab41048c35f8b9
wg-hmrc <107496826+wg-hmrc@users.noreply.github.com>
2023-03-10 12:36:07 +0000

    [MMCA-3834 ] updating play-frontend-hmrc to latest version (#70)
    * updating play-frontend-hmrc to latest version
    
    * updating package to latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.24.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.32.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "6.8.0-play-28",
     "org.typelevel" %% "cats-core" % "2.3.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.70.0",
     "org.webjars.npm" % "moment" % "2.27.0",
