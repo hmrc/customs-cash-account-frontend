ca15f328cc49df3e0369c56f5de8c6f3c59b82e1
bsgangapat <106598990+bsgangapat@users.noreply.github.com>
2023-05-16 12:19:05 +0100

    MMCA-3999 update latest libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,11 +7,11 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.15.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.3.0-play-28",
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.7.0-play-28",
     "org.typelevel" %% "cats-core" % "2.3.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.1.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.2.0",
     "org.webjars.npm" % "moment" % "2.27.0",
-    "uk.gov.hmrc" %% "tax-year" % "3.1.0"
+    "uk.gov.hmrc" %% "tax-year" % "3.2.0"
   )
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.15.0" % Test,
