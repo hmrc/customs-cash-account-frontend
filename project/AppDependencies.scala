944ca27f0014fa1b843db1b5ed1b35206153d472
wg-hmrc <waigwa.gatamu@digital.hmrc.gov.uk>
2023-08-11 16:33:55 +0100

    [MMCA-4173] Upgrade version for Non-HMRC dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,16 +8,16 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.19.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
-    "org.typelevel" %% "cats-core" % "2.3.0",
+    "org.typelevel" %% "cats-core" % "2.9.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
-    "org.webjars.npm" % "moment" % "2.27.0",
+    "org.webjars.npm" % "moment" % "2.29.4",
     "uk.gov.hmrc" %% "tax-year" % "3.2.0"
   )
   val test = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.19.0" % Test,
-    "org.jsoup" % "jsoup" % "1.10.2" % Test,
+    "org.jsoup" % "jsoup" % "1.16.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.16.46" % Test
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.14" % Test
   )
 }
