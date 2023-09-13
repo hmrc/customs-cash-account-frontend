fd02ccc85c6b893dcbf5ac6551fd1e957777ded0
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-09-13 07:19:31 +0100

    MMCA-4259 : Code change for version upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,20 +1,19 @@
-import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "7.22.0"
+
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "7.19.0",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.14.0-play-28",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.19.0-play-28",
     "org.typelevel" %% "cats-core" % "2.9.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "1.3.0",
     "org.webjars.npm" % "moment" % "2.29.4",
-    "uk.gov.hmrc" %% "tax-year" % "3.2.0"
+    "uk.gov.hmrc" %% "tax-year" % "3.3.0"
   )
   val test = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % "7.19.0" % Test,
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapVersion % Test,
     "org.jsoup" % "jsoup" % "1.16.1" % Test,
     "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % Test,
     "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % Test,
