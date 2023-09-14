e28e9f35b814a260457423a2fcb0f4d85b7b6978
sameerhmrc <106383455+sameerhmrc@users.noreply.github.com>
2023-09-14 11:00:03 +0100

    MMCA-4259 : Code change for version upgrade and fixing failing test case

--- test/utils/ConstraintsSpec.scala
+++ test/utils/ConstraintsSpec.scala
@@ -49,8 +49,8 @@ class ConstraintsSpec extends SpecBase with Constraints {
         result mustBe Valid
       }
 
-      "return invalid result is years are within 6" in new Setup {
-        def monthOld: LocalDate = LocalDateTime.now().minusMonths(8).toLocalDate
+      "return invalid result if request is after current month" in new Setup {
+        def monthOld: LocalDate = LocalDateTime.now().plusMonths(2).toLocalDate
         val result = beforeCurrentMonth("error.min").apply(monthOld)
         result mustBe Invalid(List(ValidationError(List("error.min"))))
       }
