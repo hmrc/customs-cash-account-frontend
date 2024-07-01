3768d55b0401d9c211e7c515193c3715c0b15e28
Ned Sakalauskis <62673820+Neozxc@users.noreply.github.com>
2024-07-01 14:07:35 +0100

    MMCA-4792 | Fix Platops's Warnings and Upgrade Library Versions (#127)
    * MMCA-4792 | Fixed platops's warnings
    
    * MMCA-4792 | Updated libraries

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,16 +7,16 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "9.10.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "10.3.0",
     "org.typelevel" %% "cats-core" % "2.10.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % "1.9.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % "2.1.0",
     "org.webjars.npm" % "moment" % "2.30.1",
-    "uk.gov.hmrc" %% "tax-year" % "4.0.0"
+    "uk.gov.hmrc" %% "tax-year" % "5.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion % Test,
     "org.jsoup" % "jsoup" % "1.17.2" % Test,
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.31" % Test
+    "org.mockito" %% "mockito-scala-scalatest" % "1.17.37" % Test
   )
 }
