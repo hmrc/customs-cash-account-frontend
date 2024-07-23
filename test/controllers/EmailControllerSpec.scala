772758b141a60f8f1bd4e57dac03b8e1c66381fd
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2024-07-23 14:23:53 +0100

    [MMCA-4791] Add tests for negative scenario

--- test/controllers/EmailControllerSpec.scala
+++ test/controllers/EmailControllerSpec.scala
@@ -45,12 +45,36 @@ class EmailControllerSpec extends SpecBase {
         status(result) mustBe OK
       }
     }
+
+    "display verify your email page when exception occurs while connector making the API call" in new Setup {
+
+      when(mockConnector.retrieveUnverifiedEmail(any))
+        .thenReturn(Future.successful(emailUnverifiedResponseWithNoEmailId))
+
+      running(app) {
+        val request = fakeRequest(GET, routes.EmailController.showUnverified().url)
+
+        val result = route(app, request).value
+        status(result) mustBe OK
+      }
+    }
   }
 
   "showUndeliverable" must {
 
     "display undeliverableEmail page" in new Setup {
       when(mockConnector.verifiedEmail(any)).thenReturn(Future.successful(emailVerifiedResponse))
+
+      running(app) {
+        val request = fakeRequest(GET, routes.EmailController.showUndeliverable().url)
+        val result = route(app, request).value
+
+        status(result) mustBe OK
+      }
+    }
+
+    "display undeliverableEmail page when exception occurs while connector is making the API call" in new Setup {
+      when(mockConnector.verifiedEmail(any)).thenReturn(Future.successful(emailVerifiedResponseWithNoEmailId))
 
       running(app) {
         val request = fakeRequest(GET, routes.EmailController.showUndeliverable().url)
@@ -67,7 +91,10 @@ class EmailControllerSpec extends SpecBase {
 
     val emailId = "test@test.com"
     val emailVerifiedResponse: EmailVerifiedResponse = EmailVerifiedResponse(Some(emailId))
+    val emailVerifiedResponseWithNoEmailId: EmailVerifiedResponse = EmailVerifiedResponse(None)
+
     val emailUnverifiedResponse: EmailUnverifiedResponse = EmailUnverifiedResponse(Some(emailId))
+    val emailUnverifiedResponseWithNoEmailId: EmailUnverifiedResponse = EmailUnverifiedResponse(None)
 
     val app: Application = application
       .overrides(
