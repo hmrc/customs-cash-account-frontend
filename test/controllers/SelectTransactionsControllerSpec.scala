77cff8dab32f4fcee5e68b11bc3fccb10e6954ff
Adam Cooper <61467301+AdamCoops@users.noreply.github.com>
2024-09-06 14:42:53 +0100

    MMCA-4891-Update the ui to reflect the first page that the new route … (#133)
    * MMCA-4891-Update the ui to reflect the first page that the new route will follow
    
    * MMCA-4891-update first ui page to include tests, the tests need tweaking later to include redirect to second page and day removal
    
    * MMCA-4891-update tests to reflect no day being requiured
    
    * MMCA-4891-Up the second new page and the tests to reflect it.
    
    * MMCA-4891-Update text, flow, ui elements, change text, btn labels, etc
    
    * MMCA-4891-Update requested results to display account number
    
    * MMCA-4891-update request to select, fix build errors and correct toomanyrequested faulkts
    
    * MMCA-4891-update routes for new pages, add end of line files and use emptyString
    
    * MMCA-4891-move spacing and update emptyStrings
    
    * MMCA-4891-increase test coverage
    
    * MMCA-4891-extract complex logic into private defs
    
    * MMCA-4891-update html as requested
    
    * MMCA-4891-update view names and add summaryList Spec for text changes
    
    * MMCA-4891-format summaryListSpec spacing and remove scalastyle errors

---
+++ test/controllers/SelectTransactionsControllerSpec.scala
@@ -0,0 +1,209 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import connectors.CustomsFinancialsApiConnector
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.when
+import play.api.Application
+import play.api.inject.bind
+import play.api.mvc.{AnyContentAsEmpty, AnyContentAsFormUrlEncoded}
+import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import repositories.RequestedTransactionsCache
+import utils.SpecBase
+
+import java.time.LocalDate
+import scala.concurrent.Future
+
+class SelectTransactionsControllerSpec extends SpecBase {
+
+  "onPageLoad" should {
+    "return OK when cached data present" in new Setup {
+
+      when(mockRequestedTransactionsCache.clear(any)).thenReturn(Future.successful(true))
+
+      val request: FakeRequest[AnyContentAsEmpty.type] =
+        fakeRequest(GET, routes.SelectTransactionsController.onPageLoad().url)
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe OK
+      }
+    }
+
+    "return OK when no cached data present" in new Setup {
+      when(mockRequestedTransactionsCache.clear(any)).thenReturn(Future.successful(true))
+
+      val request: FakeRequest[AnyContentAsEmpty.type] =
+        fakeRequest(GET, routes.SelectTransactionsController.onPageLoad().url)
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe OK
+      }
+    }
+
+    "return OK when clearing cache" in new Setup {
+      when(mockRequestedTransactionsCache.clear(any)).thenReturn(Future.successful(true))
+
+      val store: FakeRequest[AnyContentAsEmpty.type] =
+        fakeRequest(GET, routes.SelectTransactionsController.onSubmit().url)
+
+      val clear: FakeRequest[AnyContentAsEmpty.type] =
+        fakeRequest(GET, routes.SelectTransactionsController.onPageLoad().url)
+
+      running(app) {
+        val result = route(app, store).value
+        val test = route(app, clear).value
+        status(result) mustBe OK
+        status(test) mustBe OK
+      }
+    }
+  }
+
+  "onSubmit" should {
+    "redirect to requested transactions page when valid data has been submitted" in new Setup {
+      when(mockRequestedTransactionsCache.set(any, any)).thenReturn(Future.successful(true))
+
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        fakeRequest(POST, routes.SelectTransactionsController.onSubmit().url)
+          .withFormUrlEncodedBody("start.day" -> day, "start.month" -> "11", "start.year" -> "2020",
+            "end.day" -> "10", "end.month" -> "11", "end.year" -> "2020")
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe SEE_OTHER
+        redirectLocation(result).value mustBe routes.SelectedTransactionsController.onPageLoad().url
+      }
+    }
+
+    "return BAD_REQUEST when the start date is earlier than system start date" in new Setup {
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        fakeRequest(POST, routes.SelectTransactionsController.onSubmit().url)
+          .withFormUrlEncodedBody("start.day" -> day, "start.month" -> "9", "start.year" -> "2019",
+            "end.day" -> "10", "end.month" -> "10", "end.year" -> "2019")
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "return BAD_REQUEST when the end date is earlier than system start date" in new Setup {
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        fakeRequest(POST, routes.SelectTransactionsController.onSubmit().url)
+          .withFormUrlEncodedBody("start.day" -> day, "start.month" -> "10", "start.year" -> "2019",
+            "end.day" -> "1", "end.month" -> "9", "end.year" -> "2019")
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "return BAD_REQUEST when the start date is future date" in new Setup {
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        fakeRequest(POST, routes.SelectTransactionsController.onSubmit().url)
+          .withFormUrlEncodedBody("start.day" -> day, "start.month" -> "10", "start.year" -> "2021",
+            "end.day" -> "1", "end.month" -> "9", "end.year" -> "2019")
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "return BAD_REQUEST when the end date is future date" in new Setup {
+      val date: String = LocalDate.now().plusYears(2).getYear.toString
+
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        fakeRequest(POST, routes.SelectTransactionsController.onSubmit().url)
+          .withFormUrlEncodedBody("start.day" -> day, "start.month" -> "10", "start.year" -> "2019",
+            "end.day" -> "1", "end.month" -> "10", "end.year" -> date)
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "return BAD_REQUEST when the start date is after the end date" in new Setup {
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        fakeRequest(POST, routes.SelectTransactionsController.onSubmit().url)
+          .withFormUrlEncodedBody("start.day" -> day, "start.month" -> "11", "start.year" -> "2019",
+            "end.day" -> "1", "end.month" -> "10", "end.year" -> "2019")
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "return BAD_REQUEST when the requested data exceeds 6 years in the past" in new Setup {
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        fakeRequest(POST, routes.SelectTransactionsController.onSubmit().url)
+          .withFormUrlEncodedBody("start.day" -> "1", "start.month" -> "10", "start.year" -> "2000",
+            "end.day" -> "1", "end.month" -> "10", "end.year" -> "2000")
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "return BAD_REQUEST when invalid data submitted" in new Setup {
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        fakeRequest(POST, routes.SelectTransactionsController.onSubmit().url)
+          .withFormUrlEncodedBody("start.day" -> day, "start.invalid" -> "10", "start.year" -> "2019",
+            "end.day" -> "1", "end.month" -> "10", "end.year" -> "2019")
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+
+    "return BAD_REQUEST when start date and end date are empty" in new Setup {
+      val request: FakeRequest[AnyContentAsFormUrlEncoded] =
+        fakeRequest(POST, routes.SelectTransactionsController.onSubmit().url)
+          .withFormUrlEncodedBody("start.day" -> day, "start.month" -> "", "start.year" -> "2019",
+            "end.day" -> "1", "end.month" -> "", "end.year" -> "2019")
+
+      running(app) {
+        val result = route(app, request).value
+        status(result) mustBe BAD_REQUEST
+      }
+    }
+  }
+
+  trait Setup {
+    val mockCustomsFinancialsApiConnector: CustomsFinancialsApiConnector = mock[CustomsFinancialsApiConnector]
+    val mockRequestedTransactionsCache: RequestedTransactionsCache = mock[RequestedTransactionsCache]
+
+    val day: String = "1"
+
+    val app: Application = application
+      .overrides(
+        bind[CustomsFinancialsApiConnector].toInstance(mockCustomsFinancialsApiConnector),
+        bind[RequestedTransactionsCache].toInstance(mockRequestedTransactionsCache)
+      )
+      .configure(
+        "features.fixed-systemdate-for-tests" -> "true")
+      .build()
+  }
+}
---
+++ test/views/components/H2Spec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.components
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import play.api.Application
+import play.api.i18n.Messages
+import utils.SpecBase
+import views.html.components.h2
+
+class H2Spec extends SpecBase {
+
+  "component" should {
+
+   "display correct contents" when {
+     "h2 component has extra content" in new Setup {
+       h2Component.getElementById(id).text() mustBe s"$msg $extraContent"
+       h2Component.getElementsByClass(classes).size() mustBe 1
+     }
+
+     "h2 component can exist without extra content" in new Setup {
+       h2ComponentWithoutContent.getElementById(idNoContent).text() mustBe msg
+       h2ComponentWithoutContent.getElementsByClass(classes).size() mustBe 1
+     }
+   }
+  }
+
+  trait Setup {
+    val app: Application = application.build()
+    implicit val msgs: Messages = messages(app)
+
+    val msg: String = "some message"
+    val id: String = "test_id"
+    val idNoContent: String = "test_id_noContent"
+    val classes: String = "govuk-heading-m"
+    val extraContent: String = "some extra content"
+
+    val h2Component: Document =
+      Jsoup.parse(app.injector.instanceOf[h2].apply(msg, Some(id), classes, Some(extraContent)).body)
+
+    val h2ComponentWithoutContent: Document =
+      Jsoup.parse(app.injector.instanceOf[h2].apply(msg, Some(idNoContent), classes).body)
+  }
+}
