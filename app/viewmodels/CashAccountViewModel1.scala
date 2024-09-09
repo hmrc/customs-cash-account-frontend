08031f0da08911daf0fe182bf5a02ee51df03c1c
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2024-09-09 09:17:21 +0100

    [MMCA-4850] Add new view model skeleton

---
+++ app/viewmodels/CashAccountViewModel1.scala
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels
+
+import models.CashAccount
+import models.domain.EORI
+import utils.Utils.emptyString
+
+class CashAccountViewModel1(pageTitle: String,
+                            backLink: String,
+                           dashboard: CashAccountDashboardViewModel)
+
+object CashAccountViewModel1 {
+
+  def apply(eori: EORI,
+            account: CashAccount,
+            cashTrans: CashTransactions): CashAccountViewModel1 = {
+    CashAccountViewModel1(pageTitle = emptyString, backLink = emptyString)
+  }
+}
