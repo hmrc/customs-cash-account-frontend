ebacb1b0613624170221a57fb2b417f7cc920b02
jamie2210 <116884495+jamie2210@users.noreply.github.com>
2024-09-24 12:19:03 +0100

    [MMCA-4296] - pass test for views

--- test/views/CashAccountNoTransactionsV2Spec.scala
+++ test/views/CashAccountNoTransactionsV2Spec.scala
@@ -1,11 +1,29 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package views
 
+import config.AppConfig
 import models.{AccountStatusOpen, CDSCashBalance, CashAccount, CashAccountViewModel}
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import org.scalatest.Assertion
 import play.api.Application
 import play.api.i18n.Messages
-import utils.Utils.singleSpace
+import utils.Utils.{period, singleSpace}
 import views.html.cash_account_no_transactions_v2
 
 class CashAccountNoTransactionsV2Spec extends ViewTestHelper {
@@ -14,7 +32,7 @@ class CashAccountNoTransactionsV2Spec extends ViewTestHelper {
 
     "display correct contents" in new Setup {
       val viewDoc: Document = view(model)
-      
+
       titleShouldBeCorrect(viewDoc, "cf.cash-account.detail.title")
       shouldContainBackLinkUrl(viewDoc, appConfig.customsFinancialsFrontendHomepage)
 
@@ -22,15 +40,16 @@ class CashAccountNoTransactionsV2Spec extends ViewTestHelper {
       shouldDisplayPayImportDutyAndTaxesGuidance(viewDoc)
       shouldDisplayTopUpGuidance(viewDoc)
       shouldDisplayFindOutHowLink(viewDoc)
+      shouldDisplayHelpAndSupportGuidanceHeader(viewDoc)
       shouldDisplayHelpAndSupportGuidance(viewDoc)
     }
 
   }
 
   private def shouldDisplayCorrectAccountDetails(viewDoc: Document, accNumber: String)(implicit msgs: Messages) = {
-    viewDoc.getElementById("cash-account-number").text mustBe msgs("cf.cash-account.detail.account", accNumber)
-    viewDoc.getElementById("cash-account-header").text mustBe msgs("cf.cash-account.detail.title")
-    viewDoc.getElementById("zero-balance-display").text() mustBe s"£0$singleSpace${msgs("cf.cash-account.detail.available")}"
+    viewDoc.getElementById("account-number").text mustBe msgs("cf.cash-account.detail.account", accNumber)
+    viewDoc.getElementsByTag("h1").text mustBe msgs("cf.cash-account.detail.title")
+    viewDoc.getElementById("balance-available").text() mustBe s"£0$singleSpace${msgs("cf.cash-account.detail.available")}"
   }
 
   private def shouldDisplayPayImportDutyAndTaxesGuidance(viewDoc: Document)(implicit msgs: Messages) = {
@@ -41,25 +60,43 @@ class CashAccountNoTransactionsV2Spec extends ViewTestHelper {
     viewDoc.getElementById("cash-account-authorise-agent-guidance").text() mustBe msgs("cf.cash-account.top-up.guidance")
   }
 
-  private def shouldDisplayFindOutHowLink(viewDoc: Document)(implicit msgs: Messages) = {
-    viewDoc.getElementById("cash-account-top-up-guidance-pre").text() mustBe msgs("cf.cash-account.how-to-use.guidance.text.pre")
-    viewDoc.getElementById("cash-account-top-up-guidance-link").text() mustBe msgs("cf.cash-account.detail.link")
-    viewDoc.getElementById("cash-account-top-up-guidance-post").text() mustBe msgs("cf.cash-account.how-to-use.guidance.text.post")
+  private def shouldDisplayFindOutHowLink(viewDoc: Document)(implicit msgs: Messages, config: AppConfig) = {
+    
+    val elementLink = viewDoc.getElementById("cash-account-top-up-guidance-link")
+    elementLink.attribute("href").getValue mustBe config.cashAccountForCdsDeclarationsUrl
+    elementLink.text() mustBe msgs("cf.cash-account.how-to-use.guidance.link")
+    
+    viewDoc.text().contains(msgs("cf.cash-account.how-to-use.guidance.text.pre")) mustBe true
+    viewDoc.text().contains(msgs("cf.cash-account.how-to-use.guidance.link")) mustBe true
+    viewDoc.text().contains(msgs("cf.cash-account.how-to-use.guidance.text.post")) mustBe true
   }
 
-  private def shouldDisplayHelpAndSupportGuidance(viewDoc: Document)(implicit msgs: Messages) = {
+  private def shouldDisplayHelpAndSupportGuidanceHeader(viewDoc: Document)(implicit msgs: Messages) = {
     viewDoc.getElementById("cash-account-help-and-support-guidance-header").text() mustBe msgs("cf.cash-account.transactions.request.support.heading")
-    viewDoc.getElementById("cash-account-help-and-support-guidance-text-pre").text() mustBe msgs("cf.cash-account.help-and-support.link.text.pre")
-    viewDoc.getElementById("cash-account-help-and-support-guidance-text-link").text() mustBe msgs("cf.cash-account.help-and-support.link.text")
-    viewDoc.getElementById("cash-account-help-and-support-guidance-text-post").text() mustBe msgs("cf.cash-account.help-and-support.link.text.post")
+  }
+
+  private def shouldDisplayHelpAndSupportGuidance(viewDoc: Document)(implicit msgs: Messages, config: AppConfig): Assertion = {
+
+    val elementLink = viewDoc.getElementsByClass("cash-account-help-and-support-guidance")
+    elementLink.get(0).getElementsByTag("a").attr("href") mustBe config.cashAccountForCdsDeclarationsUrl
+
+    elementLink.text() mustBe
+      s"${
+        msgs("cf.cash-account.help-and-support.link.text.pre")
+      }$singleSpace${
+        msgs("cf.cash-account.help-and-support.link.text")
+      }$singleSpace${
+        msgs("cf.cash-account.help-and-support.link.text.post")
+      }$period"
   }
 
 
 
   trait Setup {
     val app: Application = application.build()
-    implicit val msgs: Messages = messages(app)
-    
+    //implicit val msgs: Messages = messages(app)
+    //implicit val appConfig: AppConfig = app.injector.instanceOf[AppConfig]
+
     val eori = "test_eori"
     val balances: CDSCashBalance = CDSCashBalance(None)
     val accNumber = "12345678"
