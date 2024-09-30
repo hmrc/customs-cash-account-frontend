d7a796f5ab4f762856422a35ec98f0174dbd5e67
jamie2210 <116884495+jamie2210@users.noreply.github.com>
2024-09-30 12:04:19 +0100

    [MMCA-4956] - add test for added link class

--- test/viewmodels/CashAccountV2ViewModelSpec.scala
+++ test/viewmodels/CashAccountV2ViewModelSpec.scala
@@ -158,7 +158,8 @@ class CashAccountV2ViewModelSpec extends SpecBase {
       link = Some(hmrcNewTabLinkComponent(linkMessage = "cf.cash-account.help-and-support.link.text",
         href = config.cashAccountForCdsDeclarationsUrl,
         preLinkMessage = Some("cf.cash-account.help-and-support.link.text.pre.v2"),
-        postLinkMessage = Some("cf.cash-account.help-and-support.link.text.post")))
+        postLinkMessage = Some("cf.cash-account.help-and-support.link.text.post"),
+        classes = "govuk-body govuk-!-margin-bottom-9"))
     )
 
   }
