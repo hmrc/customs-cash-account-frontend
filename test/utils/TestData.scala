42b1c65753ac83b2b467d19889a4bead86e9d815
vineet <vineet.tyagi@digital.hmrc.gov.uk>
2024-10-02 11:40:57 +0100

    [MMCA-4949] Add constants for the vallues of the pages and add tests for MetaData

--- test/utils/TestData.scala
+++ test/utils/TestData.scala
@@ -31,4 +31,26 @@ object TestData {
 
   val TAX_TYPE: TaxType = TaxType(Some("a"), "a", AMOUNT)
   val TAX_GROUP: TaxGroup = TaxGroup(ImportVat, AMOUNT, Seq(TAX_TYPE))
+
+  val PAGE_1 = 1
+  val PAGE_2 = 2
+  val PAGE_3 = 2
+  val PAGE_4 = 4
+  val PAGE_5 = 5
+  val PAGE_6 = 6
+  val PAGE_7 = 7
+  val PAGE_8 = 8
+  val PAGE_10 = 10
+  val PAGE_11 = 11
+  val PAGE_20 = 20
+  val PAGE_21 = 21
+  val PAGE_30 = 30
+  val PAGE_40 = 40
+  val PAGE_50 = 50
+  val PAGE_51 = 51
+  val PAGE_100 = 100
+  val PAGE_101 = 101
+  val PAGE_150 = 150
+  val PAGE_201 = 201
+  val PAGE_400 = 400
 }
--- test/viewmodels/pagination/MetaDataSpec.scala
+++ test/viewmodels/pagination/MetaDataSpec.scala
@@ -17,16 +17,74 @@
 package viewmodels.pagination
 
 import utils.SpecBase
+import utils.TestData.{
+  PAGE_1, PAGE_10, PAGE_101, PAGE_11, PAGE_150, PAGE_20, PAGE_201, PAGE_21, PAGE_30, PAGE_4,
+  PAGE_40, PAGE_100, PAGE_400, PAGE_5, PAGE_50, PAGE_51, PAGE_6, PAGE_7, PAGE_8
+}
 
 class MetaDataSpec extends SpecBase {
 
   "apply" should {
 
-    "populate MetaData correctly" when {
+    "populate correct totalPages" when {
 
-      "" in {
+      "totalNumberOfMovements is not a whole number" in {
 
+        MetaData(3, 2, PAGE_1).totalPages mustBe 2
+        MetaData(PAGE_5, 2, 1).totalPages mustBe 3
+        MetaData(PAGE_10, 3, 1).totalPages mustBe 4
+      }
+
+      "totalNumberOfMovements is a whole number" in {
+
+        MetaData(2, 1, 1).totalPages mustBe 2
+        MetaData(PAGE_6, 2, 1).totalPages mustBe 3
+        MetaData(PAGE_8, 2, 1).totalPages mustBe 4
+      }
+    }
+
+    "return numberOfMovementsPerPage value" when {
+      "current page is 1" in {
+
+        MetaData(PAGE_20, PAGE_10, 1).to mustBe PAGE_10
+        MetaData(PAGE_40, PAGE_20, 1).to mustBe PAGE_20
+        MetaData(PAGE_100, PAGE_50, 1).to mustBe PAGE_50
+      }
+    }
+
+    "return totalNumberOfMovements' value" when {
+      "number of movement times by the current page number is greater than the total number of movements" in {
+
+        MetaData(PAGE_11, PAGE_10, 2).to mustBe PAGE_11
+        MetaData(PAGE_21, PAGE_10, 3).to mustBe PAGE_21
+        MetaData(PAGE_101, PAGE_50, 3).to mustBe PAGE_101
+      }
+    }
+
+    "return from value as 1" when {
+      "current page is 1" in {
+
+        MetaData(2, 1, 1).from mustBe 1
+      }
+    }
+
+    "return number of movements per page + 1" when {
+      "current page is 2" in {
+
+        MetaData(PAGE_30, PAGE_10, 2).from mustBe PAGE_11
+        MetaData(PAGE_5, 2, 2).from mustBe 3
+        MetaData(PAGE_150, PAGE_50, 2).from mustBe PAGE_51
+      }
+    }
+
+    "return (number of movements per page times by (current page minus 1)) plus 1" when {
+      "on page other than 1 and 2" in {
+
+        MetaData(PAGE_30, PAGE_10, 3).from mustBe 21
+        MetaData(PAGE_10, 2, PAGE_4).from mustBe PAGE_7
+        MetaData(PAGE_400, PAGE_50, PAGE_5).from mustBe PAGE_201
       }
     }
   }
+
 }
