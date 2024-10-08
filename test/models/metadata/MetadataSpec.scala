b9aaa4d48210d1602bd1e2d9f4c028598ab04aa4
Ned Sakalauskis <62673820+Neozxc@users.noreply.github.com>
2024-10-08 13:34:21 +0100

    MMCA-4951 | Added unit tests for Sdes Metadata

---
+++ test/models/metadata/MetadataSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.metadata
+
+import play.api.libs.json.Json
+import utils.SpecBase
+
+import play.api.libs.json._
+
+class MetadataSpec extends SpecBase {
+
+  "MetadataSpec" must {
+
+    "convert items to map correctly" in {
+      val metadataItems = Seq(MetadataItem("key1", "value1"), MetadataItem("key2", "value2"))
+      val metadata = Metadata(metadataItems)
+
+      val expectedMap = Map("key1" -> "value1", "key2" -> "value2")
+      metadata.asMap mustBe expectedMap
+    }
+
+    "serialize correctly" in {
+      val metadataItems = Seq(MetadataItem("key1", "value1"), MetadataItem("key2", "value2"))
+      val metadata = Metadata(metadataItems)
+
+      val expectedJson = Json.arr(
+        Json.obj("metadata" -> "key1", "value" -> "value1"),
+        Json.obj("metadata" -> "key2", "value" -> "value2")
+      )
+
+      Json.toJson(metadata) mustBe expectedJson
+    }
+
+    "deserialize correctly" in {
+      val json = Json.arr(
+        Json.obj("metadata" -> "key1", "value" -> "value1"),
+        Json.obj("metadata" -> "key2", "value" -> "value2")
+      )
+
+      val expectedMetadataItems = Seq(MetadataItem("key1", "value1"), MetadataItem("key2", "value2"))
+      json.as[Metadata] mustBe Metadata(expectedMetadataItems)
+    }
+  }
+}
---
+++ test/models/metadata/SdesFileMetadataSpec.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models.metadata
+
+import models.{FileFormat, FileRole}
+import utils.SpecBase
+
+class SdesFileMetadataSpec extends SpecBase {
+
+  "correctly convert CashStatementFileMetadata to a Map" in new Setup {
+    val metadata: CashStatementFileMetadata = CashStatementFileMetadata(
+      periodStartYear = periodStartYear,
+      periodStartMonth = periodStartMonth,
+      periodStartDay = periodStartDay,
+      periodEndYear = periodEndYear,
+      periodEndMonth = periodEndMonth,
+      periodEndDay = periodEndDay,
+      fileFormat = FileFormat.Pdf,
+      fileRole = FileRole(fileRoleName),
+      statementRequestId = statementRequestId
+    )
+
+    val result: Map[String, String] = metadata.toMap
+
+    result mustEqual Map(
+      "periodStartYear" -> s"$periodStartYear",
+      "periodStartMonth" -> s"$periodStartMonth",
+      "periodStartDay" -> s"$periodStartDay",
+      "periodEndYear" -> s"$periodEndYear",
+      "periodEndMonth" -> s"$periodEndMonth",
+      "periodEndDay" -> s"$periodEndDay",
+      "fileFormat" -> s"${FileFormat.Pdf}",
+      "fileRole" -> s"${FileRole(fileRoleName)}",
+      "statementRequestId" -> s"$statementRequestId"
+    )
+  }
+
+  trait Setup {
+    val periodStartYear = 2024
+    val periodStartMonth = 10
+    val periodStartDay = 1
+    val periodEndYear = 2024
+    val periodEndMonth = 10
+    val periodEndDay = 31
+    val fileRoleName = "CashStatement"
+    val statementRequestId: Option[String] = Some("requestId")
+  }
+}
