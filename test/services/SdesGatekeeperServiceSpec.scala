d185644b6402b2dfa04eda43eab192aaf87e708a
Ned Sakalauskis <62673820+Neozxc@users.noreply.github.com>
2024-10-09 11:59:42 +0100

    MMCA-4951 | Added unit test for convertTo

--- test/services/SdesGatekeeperServiceSpec.scala
+++ test/services/SdesGatekeeperServiceSpec.scala
@@ -18,7 +18,9 @@ package services
 
 import models.*
 import models.FileRole.CashStatement
-import models.metadata.{Metadata, MetadataItem}
+import models.metadata.{CashStatementFileMetadata, Metadata, MetadataItem}
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.{times, verify, when}
 import play.api.Application
 import play.api.i18n.Messages
 import utils.SpecBase
@@ -43,6 +45,25 @@ class SdesGatekeeperServiceSpec extends SpecBase {
       result.metadata.fileFormat mustBe FileFormat(csv)
       result.metadata.fileRole mustBe CashStatement
       result.metadata.statementRequestId mustBe someRequestId
+    }
+  }
+
+  "SdesGatekeeperService.convertTo" should {
+
+    "correctly convert FileInformation to a sequence of a generic type" in new Setup {
+      val mockConverter: FileInformation => CashStatementFile = mock[FileInformation => CashStatementFile]
+      val sdesFiles: Seq[FileInformation] = Seq(validCashStatementFileInformation, validCashStatementFileInformation)
+
+      when(mockConverter.apply(ArgumentMatchers.any[FileInformation]())).thenReturn(validCashStatementFile)
+
+      val result: Seq[CashStatementFile] = sdesGatekeeperService.convertTo[CashStatementFile](mockConverter)(sdesFiles)
+
+      result.length mustBe sdesFiles.length
+      result.foreach { file =>
+        file mustBe validCashStatementFile
+      }
+
+      verify(mockConverter, times(sdesFiles.length)).apply(ArgumentMatchers.any[FileInformation]())
     }
   }
 
@@ -79,8 +100,21 @@ class SdesGatekeeperServiceSpec extends SpecBase {
         MetadataItem("FileType", csv),
         MetadataItem("FileRole", "CashStatement"),
         MetadataItem("CashAccountNumber", someAccountNumber.getOrElse(emptyString)),
-        MetadataItem("statementRequestID", someRequestId.getOrElse(emptyString))
-      ))
-    )
+        MetadataItem("statementRequestID", someRequestId.getOrElse(emptyString)))))
+
+    val validCashStatementFile: CashStatementFile = CashStatementFile(
+      filename = fileName,
+      downloadURL = downloadURL,
+      size = fileSize,
+      metadata = CashStatementFileMetadata(
+        periodStartYear = periodStartYear,
+        periodStartMonth = periodStartMonth,
+        periodStartDay = periodStartDay,
+        periodEndYear = periodEndYear,
+        periodEndMonth = periodEndMonth,
+        periodEndDay = periodEndDay,
+        fileFormat = FileFormat(csv),
+        fileRole = CashStatement,
+        statementRequestId = someRequestId), emptyString)
   }
 }
