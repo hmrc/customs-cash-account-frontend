9eddb99a675aac24f57ad3308cc22861e07fc189
jack-brannon <166420733+Jack-Brannon@users.noreply.github.com>
2025-11-04 15:16:52 +0000

    DCWL-3996 | Dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,9 +7,9 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.8.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.19.0",
     "org.typelevel"     %% "cats-core"                  % "2.12.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % "2.7.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % "2.10.0",
     "org.webjars.npm"    % "moment"                     % "2.30.1",
     "uk.gov.hmrc"       %% "tax-year"                   % "5.0.0"
   )
